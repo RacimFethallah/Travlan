dac4789fef0f8f52b24aebbe718a58b6d2a19a64
celenablh <127396219+celenablh@users.noreply.github.com>
2023-06-10 13:02:48 +0100

    Add files via upload

---
+++ criteria_search.js
@@ -0,0 +1,48 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    if (location.href.indexOf('criteria_search.php') !== -1) {
+        body.className = 'search';
+
+
+        const urlParams1 = new URLSearchParams(window.location.search);
+        const dataParam1 = urlParams1.get('data');
+
+
+        if (dataParam1) {
+            // Decode the parameter and parse the JSON data
+
+            const queryString1 = window.location.search;
+            const urlParams1 = new URLSearchParams(queryString1);
+            const searchQuery1 = urlParams1.get('data');
+            //const decodedData = decodeURIComponent(searchQuery);
+            //const data = JSON.parse(decodedData);
+
+            // Access the value of the "data" parameter
+            //const dd = data;
+             
+            
+            // Now you can work with the retrieved data
+            fetch('includes/criteria_search1.php', {
+                method: 'POST',
+                body: new URLSearchParams({
+                    searchQuery1 : searchQuery1
+                })
+            })
+                .then(response => response.json())
+                .then(data => {
+                    displaySearchResults(data);
+                })
+                .catch(error => {
+                    // Handle any errors that occurred during the fetch request
+                    console.error('Error:', error);
+                });
+        }
+
+
+
+
+    }
+
+})
---
+++ criteria_search.php
@@ -0,0 +1,79 @@
+<?php
+include_once 'header.php';
+?> 
+
+
+
+<aside id="asideSearchPage">
+    <!-- Filter section on the left -->
+    <h2>
+        Budget
+    </h2>
+        <!-- Filter options, checkboxes, dropdowns, etc. -->
+        <label>
+            Budget Total 
+        </label>
+        <span class="argent"> </span>
+        <label>
+            Budget Hebergement
+        </label>
+        <span class="argent"> </span>
+        <label>
+            Budget Restaurant
+        </label>
+        <span class="argent"> </span>
+        <label>
+            Budget Transport
+        </label>
+        <span class="argent">  </span>
+        <label>
+            Budget Extra
+        </label>
+        <span class="argent"> </span>
+        <!-- Additional filters -->
+</aside>
+
+
+
+<div class="content-wrapper">
+    <section id="sectionSearchPage1">
+        <form action="" method="POST" id="searchform">
+         <input type='submit' id='hebergement1' value="Hebergement"> 
+         <input type='submit' id='restaurant1' value="Restaurants"> 
+         <input type='submit' id='activite1' value="Activites"> 
+        </form>
+    </section>
+    <div id="resultsContainerB"></div>
+
+
+    <section id="sectionSearchPage">
+    <h2>Results for <span id="resulttitle"></span> <span id="resultnumber"></span></h2>
+    <ul class="search-results">
+
+        <!-- Additional search results -->
+    </ul>
+</section>
+
+</div>
+
+
+<?php
+include_once 'login.php';
+?>
+
+<?php
+include_once 'register.php';
+?>
+
+<?php
+include_once 'passwordReset.php';
+?>
+
+<?php
+include_once 'includes/errors.php';
+?>
+
+
+<?php
+include_once 'footer.php';
+?> 
